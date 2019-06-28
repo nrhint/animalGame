ee9eedf1527731a41dbd3df5632f07679f9fd4dc
Children <Children>
2019-06-28 13:06:46 -0600

    working on filling out the classes to allow the server to work better.

--- Client.py
+++ Client.py
@@ -4,10 +4,11 @@ import socket
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
-
+##Here is the start of the game:
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
+    ##Sending required data to the server:
+    data = input('what is your player name: ')
+    s.sendall(data.encode('utf-8'))
     while True:
-        data = input("inputMessage: ")
-        s.sendall(data.encode('utf-8'))
-        #data = s.recv(1024)
+        pass
--- Server.py
+++ Server.py
@@ -5,17 +5,36 @@ print("server starting...")
 import socket
 from threading import Thread
 
+players = []
+
 class ClientThread(Thread):
     def __init__(self, ip, port):
         Thread.__init__(self)
         self.ip = ip
         self.port = port
         print("[+] New thread started for %s:%s"%(ip, str(port)))
+        self.state = 'init'
     def run(self):
         while True:
-            data = conn.recv(2048)
-            data = data.decode()
-            print(data+"--FROM--:"+self.ip)
+            if self.state == 'init':
+                
+                data = conn.recv(2048).decode()
+                players.append(Player(str(data)))
+            elif self.state == 'getData':
+                pass
+            elif self.state == 'SendData':
+                pass
+##            data = conn.recv(2048)
+##            data = data.decode()
+##            print(data)
+
+class Player:
+    def __init__(self, name):
+        self.health = 100
+        self.name = name
+        self.speed = 2
+        self.type = 'crab'
+        print("!!!PLAYER %s CREATED!!!"%self.name)
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)

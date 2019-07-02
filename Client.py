a14935dc17e4a831dcbe1feffe1c5b45b6c1cd54
nrhint <nathan@hintonclan.org>
2019-07-02 17:59:26 +0100

    Fixed merge isues

--- Client.py
+++ Client.py
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-##Nathan Hinton
-
-import socket
-
-HOST = '192.168.85.56'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    while True:
-        data = input("inputMessage: ")
-        s.sendall(data.encode('utf-8'))
-        #data = s.recv(1024)
-=======
 ##Nathan Hinton
 
 import socket
@@ -28,4 +13,3 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         data = input("Command: ")
         s.sendall(data.encode())
->>>>>>> 406a32634192f4950cbfeb0fb83cc91999957251
